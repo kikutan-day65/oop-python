78e6ff705b6c054083d3ccb7e7e40b2d40308f01
Haruto Mori <baystarsman@outlook.jp>
2023-03-08 10:46:58 +0100

    create a new global attribute

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 class Item:
+
+    pay_rate = 0.8  # The pay rate after 20% discount (global class attribute)
+
     def __init__(self, name: str, price: float, quantity: int):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to 0!"
@@ -19,5 +22,9 @@ print("name:", item1.name)
 print("price:", item1.price)
 print("quantity:", item1.quantity)
 print("total price:", item1.calculate_total_price())
+
+# try to find attribute at instance level, and not found
+# then try to find it at class level
+print(item1.pay_rate)
 
 print()
